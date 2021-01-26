5770c4d4851039507fae66635e34e245480e174f
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-01-26 19:39:58 +0100

    Test 1 - funkar inte ännu

---
+++ Seminarium1_uppgift6.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main() {
+  double in[] = {11.0, 20.0, 100.0}; 
+  double out[3];
+
+  square_reverse(in, out, 3);
+
+  printf("pos 0 = %.1f\npos 1 = %.1f\npos 2 = %.1f\n", out[0], out[1], out[2]);
+  
+  return 0;
+}
+
+void square_reverse(double *x, double *y, int len){
+  int i = 0;
+  while(i < len){
+    y[len] = x[i] * x[i];
+    i++;
+  }
+}
