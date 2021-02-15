2cdd6e9ecc29b5a01da550a9c5831c3f7548b0a9
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-02-15 10:31:03 +0100

    commit

--- LAB3/hello-leds-switches/main.c
+++ LAB3/hello-leds-switches/main.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-	volatile int *push_buttons = (volatile int *)0x8000abc0;
-	volatile int *LED = (volatile int *)0x80007bc0;
+	volatile int* switches = (volatile int*) 0x8000abc0;
+	TRISD = 0xFF00; // set RD[7:0] to output,
+	// RD[11:8] to input
 	while (1)
 	{
-		if ((*push_buttons) & 8)
-			*LED = 0x3f;
-		else
-			*LED = 0x0;
+		switches = (PORTD >> 8) & 0xF; // Read and mask switches from
+		// RD[11:8]
+		PORTD = switches; // display on the LEDs
 	}
 }
