3f319a9f2db0b20682ffa7bf25ff99091ae6db4d
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-03-01 18:29:18 +0100

    commit

--- Mini_Projekt/project-template-master/views.c
+++ Mini_Projekt/project-template-master/views.c
@@ -40,19 +40,28 @@ void menu()
     */
 
 }
+void write_highscore(){
+    display_string(0, "  Enter your initials ");
+    display_string(1, "");
+    display_string(2, "");
+    display_string(3, ""+);
+    display_update();
+    display_update();
+}
 
 void highscore()
 {
-    int btn = getbtns();
+    // int btn = getbtns();
 
     display_string(0, "  Highscore ");
-    display_string(1, "1    ");
-    display_string(2, "2    ");
-    display_string(3, "3  ");
+    display_string(1, "1:     ");
+    display_string(2, "2:     ");
+    display_string(3, "3:   ");
     display_update();
     display_update();
-    if (btn & 0x1){
+    /* if (btn & 0x1){
         menu();
     }
+    */ // To be added back if Views does not work.
     
 }
