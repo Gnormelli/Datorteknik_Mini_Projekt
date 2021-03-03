b58475e2821c9a139057182d5983abe30ddaa16a
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-03-03 18:15:35 +0100

    commit

--- Mini_Projekt/project-template-master/mipslabmain.c
+++ Mini_Projekt/project-template-master/mipslabmain.c
@@ -7,29 +7,32 @@
 
    For copyright and licensing, see file COPYING */
 
-#include <stdint.h>   /* Declarations of uint_32 and the like */
-#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
-#include "mipslab.h"  /* Declatations for these labs */
+#include <stdint.h>	 /* Declarations of uint_32 and the like */
+#include <pic32mx.h> /* Declarations of system-specific addresses etc */
+#include "mipslab.h" /* Declatations for these labs */
 
-int main(void) {
-        /*
+int main(void)
+{
+	/*
 	  This will set the peripheral bus clock to the same frequency
 	  as the sysclock. That means 80 MHz, when the microcontroller
 	  is running at 80 MHz. Changed 2017, as recommended by Axel.
 	*/
-	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
-	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
-	while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
+	SYSKEY = 0xAA996655; /* Unlock OSCCON, step 1 */
+	SYSKEY = 0x556699AA; /* Unlock OSCCON, step 2 */
+	while (OSCCON & (1 << 21))
+		;				  /* Wait until PBDIV ready */
 	OSCCONCLR = 0x180000; /* clear PBDIV bit <0,1> */
-	while(OSCCON & (1 << 21));  /* Wait until PBDIV ready */
-	SYSKEY = 0x0;  /* Lock OSCCON */
-	
+	while (OSCCON & (1 << 21))
+		;		  /* Wait until PBDIV ready */
+	SYSKEY = 0x0; /* Lock OSCCON */
+
 	/* Set up output pins */
 	AD1PCFG = 0xFFFF;
 	ODCE = 0x0;
 	TRISECLR = 0xFF;
 	PORTE = 0x0;
-	
+
 	/* Output pins for display signals */
 	PORTF = 0xFFFF;
 	PORTG = (1 << 9);
@@ -37,18 +40,18 @@ int main(void) {
 	ODCG = 0x0;
 	TRISFCLR = 0x70;
 	TRISGCLR = 0x200;
-	
+
 	/* Set up input pins */
 	TRISDSET = (1 << 8);
 	TRISFSET = (1 << 1);
-	
+
 	/* Set up SPI as master */
 	SPI2CON = 0;
 	SPI2BRG = 4;
 	/* SPI2STAT bit SPIROV = 0; */
 	SPI2STATCLR = 0x40;
 	/* SPI2CON bit CKP = 1; */
-        SPI2CONSET = 0x40;
+	SPI2CONSET = 0x40;
 	/* SPI2CON bit MSTEN = 1; */
 	SPI2CONSET = 0x20;
 	/* SPI2CON bit ON = 1; */ /* Turn on SPI */
@@ -63,21 +66,56 @@ int main(void) {
 	// I2C1CONSET = 1 << 13; //SIDL = 1
 	// I2C1CONSET = 1 << 15; // ON = 1
 	// temp = I2C1RCV; //Clear receive buffer <-- needs change!!!!
-	
+
 	display_init();
 	//display_string(0, "Welcome to");
 	//display_string(1, "TETRIS");
 	//display_string(2, "Engineering");
 	//display_string(3, "Welcome!");
 	display_update();
-	
-	// display_image(96, icon);
-	
-	labinit(); /* Do any lab-specific initialization */
 
-	while( 1 )
+	// display_image(96, icon);
+
+	labinit(); /* Do any lab-specific initialization */
+	/*
+  if (views == 1)
+  {
+    menu();
+    views = 0;
+    return;
+  }
+
+  if (views == 0)
+  {
+    if (btn & 0x4) // Start to play the game, WIP, needs F port, mapped to BTN3 for now
+    {
+      views = 2;
+      gameboard();
+      while (views == 2)
+      {
+        play();
+      }
+      return;
+    }
+    if (btn & 0x2) //  Highscore
+    {
+      views = 5;
+      highscore();
+      return;
+    }
+  }
+  if (views == 5)
+  {
+    if (btn & 0x2)
+    {
+      views = 1;
+      return;
+    }
+  }
+*/
+	while (1)
 	{
-	  labwork(); /* Do lab-specific things again and again */
+		labwork(); /* Do lab-specific things again and again */
 	}
 	return 0;
 }
--- Mini_Projekt/project-template-master/views.c
+++ Mini_Projekt/project-template-master/views.c
@@ -10,39 +10,12 @@ void titleview()
 
 void menu()
 {
-    // Improve with a delay
-
-    /*
-    int btn = 0;
-    int btn = getbtns();
-
     
-    if (btn & 0x4) 
-    {
-        display_string(0, "    ");
-        display_string(1, "PLAY-BTN2");
-        display_string(2, "HIGHSCORE-BTN3");
-        display_string(3, "    ");
-        display_update();
-    }
-    if (btn & 0x2)
-    */
     display_string(0, "    ");
     display_string(1, "  PLAY - BTN2");
     display_string(2, "  HIGHSCORE - BTN3");
     display_string(3, "    ");
     display_update();
-
-    /* int btn = getbtns();
-
-    if (btn & 0x4){
-        highscore();
-    }
-    if (btn & 0x1)
-    {
-        play();
-    }
-    */
 }
 void write_highscore()
 {
@@ -68,17 +41,9 @@ void gameover(char *scorescreen[], int *score)
 
 void highscore()
 {
-    // int btn = getbtns();
-
     display_string(0, "  Highscore ");
     display_string(1, "1:     ");
     display_string(2, "2:     ");
     display_string(3, "3:   ");
     display_update();
-    display_update();
-    /* if (btn & 0x1){
-        menu();
-    }
-    */
-    // To be added back if Views does not work.
 }
