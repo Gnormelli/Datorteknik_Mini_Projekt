07217f7af2e1ca548d6ffda47a34e1065d437ab0
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-03-03 14:03:24 +0100

    commit

--- Mini_Projekt/project-template-master/mipslabmain.c
+++ Mini_Projekt/project-template-master/mipslabmain.c
@@ -7,32 +7,29 @@
 
    For copyright and licensing, see file COPYING */
 
-#include <stdint.h>	 /* Declarations of uint_32 and the like */
-#include <pic32mx.h> /* Declarations of system-specific addresses etc */
-#include "mipslab.h" /* Declatations for these labs */
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "mipslab.h"  /* Declatations for these labs */
 
-int main(void)
-{
-	/*
+int main(void) {
+        /*
 	  This will set the peripheral bus clock to the same frequency
 	  as the sysclock. That means 80 MHz, when the microcontroller
 	  is running at 80 MHz. Changed 2017, as recommended by Axel.
 	*/
-	SYSKEY = 0xAA996655; /* Unlock OSCCON, step 1 */
-	SYSKEY = 0x556699AA; /* Unlock OSCCON, step 2 */
-	while (OSCCON & (1 << 21))
-		;				  /* Wait until PBDIV ready */
+	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
+	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
+	while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
 	OSCCONCLR = 0x180000; /* clear PBDIV bit <0,1> */
-	while (OSCCON & (1 << 21))
-		;		  /* Wait until PBDIV ready */
-	SYSKEY = 0x0; /* Lock OSCCON */
-
+	while(OSCCON & (1 << 21));  /* Wait until PBDIV ready */
+	SYSKEY = 0x0;  /* Lock OSCCON */
+	
 	/* Set up output pins */
 	AD1PCFG = 0xFFFF;
 	ODCE = 0x0;
 	TRISECLR = 0xFF;
 	PORTE = 0x0;
-
+	
 	/* Output pins for display signals */
 	PORTF = 0xFFFF;
 	PORTG = (1 << 9);
@@ -40,18 +37,18 @@ int main(void)
 	ODCG = 0x0;
 	TRISFCLR = 0x70;
 	TRISGCLR = 0x200;
-
+	
 	/* Set up input pins */
 	TRISDSET = (1 << 8);
 	TRISFSET = (1 << 1);
-
+	
 	/* Set up SPI as master */
 	SPI2CON = 0;
 	SPI2BRG = 4;
 	/* SPI2STAT bit SPIROV = 0; */
 	SPI2STATCLR = 0x40;
 	/* SPI2CON bit CKP = 1; */
-	SPI2CONSET = 0x40;
+        SPI2CONSET = 0x40;
 	/* SPI2CON bit MSTEN = 1; */
 	SPI2CONSET = 0x20;
 	/* SPI2CON bit ON = 1; */ /* Turn on SPI */
@@ -66,25 +63,21 @@ int main(void)
 	// I2C1CONSET = 1 << 13; //SIDL = 1
 	// I2C1CONSET = 1 << 15; // ON = 1
 	// temp = I2C1RCV; //Clear receive buffer <-- needs change!!!!
-
+	
 	display_init();
 	//display_string(0, "Welcome to");
 	//display_string(1, "TETRIS");
 	//display_string(2, "Engineering");
 	//display_string(3, "Welcome!");
 	display_update();
-
+	
 	// display_image(96, icon);
-
+	
 	labinit(); /* Do any lab-specific initialization */
 
-	titleview();
-	delay(3000);
-	views = 1;
-
-	while (1)
+	while( 1 )
 	{
-	  labwork();
+	  labwork(); /* Do lab-specific things again and again */
 	}
 	return 0;
 }
--- Mini_Projekt/project-template-master/mipslabwork.c
+++ Mini_Projekt/project-template-master/mipslabwork.c
@@ -2,14 +2,10 @@
 #include <pic32mx.h> /* Declarations of system-specific addresses etc */
 #include "mipslab.h" /* Declatations for these labs */
 
-#define TMR2PERIOD (80000000 / 256)
+#define TMR2PERIOD (80000000 /10/ 256)
 
 volatile int *TRIS_E; // declare the pointers volatile and global
 volatile int *PORT_E; // DONT!!!!! use the defenitions in pic32 sheet, numerous errors occur
-
-int views = 0; // 0 - title view, 1 - Menu view, 2 - Game view, 3 - Game over view, 4 - Write high score view, 5 - High Score view
-
-int btncounter = 0;
 
 int views = 0; // 0 - title view, 1 - Menu view, 2 - Game view, 3 - Game over view, 4 - Write high score view, 5 - High Score view
 
@@ -32,56 +28,55 @@ void labinit(void)
   *PORT_E = 0x0;
   *TRIS_E = *TRIS_E & 0xff00;
 
-  TRISD = TRISD | 0x0ff0;
+  TRISD = TRISD | 0x0fe0;
 
   TRISF = TRISF | 0x1;
-
-  titleview();
-	delay(3000);
-	views = 1;
 
   return;
 }
 
 void labwork(void)
 {
-  //if (swt != 0 && btncounter == 0) //
-  //{
-  //if (views == 1)
-  //{
-  int swt = getsw();
-  if (views == 1)
+  int btn = getbtns();
+  if (views == 0)
   {
+    display_string(2, "TETRIS");
+    display_update();
+    delay(3000);
     menu();
-    views = 0;
-    return;
+    views = 1;
   }
-  if (swt & 0x4) // Start to play the game, WIP, needs F port, mapped to BTN3 for now
+
+  if (btn != 0 && btncounter == 0) // 
   {
-    views = 2;
-    while (views == 2)
+    btncounter == 1;
+
+    if (views == 1)
     {
-      play(); /* Do lab-specific things again and again */
+      if (btn & 0x4) // Start to play the game, WIP, needs F port, mapped to BTN3 for now
+      {
+        PORTDCLR=0x0007;
+        views = 2;
+        play();
+        return;
+      }
+      if (btn & 0x2) //  Highscore
+      {
+        PORTDCLR =0x0007;
+        views = 5;
+        highscore();
+        return;
+      }
     }
-    PORTDCLR = 0x4;
-    return;
-  }
-  if (swt & 0x2) //  Highscore
-  {
-    views = 5;
-    highscore();
-    PORTDCLR = 0x2;
-    return;
-  }
-  //}
-  if (views == 5)
-  {
-    if (swt & 0x2) // Back to menu
+    if (views == 5)
     {
-      views = 1;
-      PORTDCLR = 0x2;
-      return;
+      if (btn & 0x2) // Back to menu
+      {
+        PORTDCLR = 0x0007;
+        views = 1;
+        menu();
+        return;
+      }
     }
   }
-  //}
 }
