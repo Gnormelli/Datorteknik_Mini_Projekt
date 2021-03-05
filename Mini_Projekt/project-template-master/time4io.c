f620a0a9fb3134c0b685cd20bb07be2d59efd18f
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2021-03-05 10:30:37 +0100

    commit

--- Mini_Projekt/project-template-master/time4io.c
+++ Mini_Projekt/project-template-master/time4io.c
@@ -7,8 +7,8 @@
  * switches are bits 11-8
  * slr >> 8 steps to teturn 4 LSB
  */
-int getsw(void)  // called as "getsw();"
-{ 
+int getsw(void) // called as "getsw();"
+{
 
     return (PORTD >> 8) & 0x000F;
 }
@@ -20,5 +20,10 @@ int getsw(void)  // called as "getsw();"
  */
 int getbtns(void) // called as "getbtns();"
 {
-    return (PORTD >> 5) & 0x0007;
+    int getbtns = 0;
+    getbtns = PORTD & 0x0e0;
+    getbtns = getbtns >> 4;
+    if ((PORTF >> 1) & 1)
+        getbtns = 1;
+    return getbtns;
 }
